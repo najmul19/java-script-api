5472b4de0362ce374c10239e160111e1a895aa51
najmul <mdnajmulislam10992@gmail.com>
2025-03-16 16:21:54 +0600

    added the backtick means multiple line string ans dynamic string

--- ES6-Intro/tempCodeRunnerFile.js
+++ ES6-Intro/tempCodeRunnerFile.js
@@ -1,7 +1,1 @@
-function add(num1,num2){
-    const res = num1+num2;
-    console.log(res)
-    return res;
-}
-
-const sum = add
+is
---
+++ ES6-Intro/templete.js
@@ -0,0 +1,21 @@
+const f = 'j'
+const l = 'p'
+
+// string immutable->no change
+const name  = f+' '+l;
+console.log(name)
+
+const a=10;
+const b=20;
+const res = 'The sum of ' + a +' and ' + ' '+ b + ' is ' + (a+b)
+console.log(res)
+
+const math = `the sum of ${a} and ${b} is ${a+b}`
+console.log(math)
+
+const email = `Hi John
+cena cena
+fena
+fena
+`
+console.log(email)
