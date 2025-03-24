b5dfd142ed962a50f7160a3eea1c5f8ce074edfe
najmul <mdnajmulislam10992@gmail.com>
2025-03-24 18:04:25 +0600

    display catgeory to ui btn

--- API-Project/love-tube/Js/video.js
+++ API-Project/love-tube/Js/video.js
@@ -1,5 +1,3 @@
-
-
 const loadCategories = () =>{
     fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
     .then(res=>res.json())
@@ -8,8 +6,16 @@ const loadCategories = () =>{
 
 };
 
-const displayCategories = (data) =>{
-    console.log(data)
+const displayCategories = (categories) =>{
+    const categoryContainer = document.getElementById('categories');
+    // console.log(categories)
+    categories.forEach((item) =>{
+        console.log(item)
+        const button = document.createElement('button');
+        button.classList = 'btn';
+        button.innerText = item.category;
+        categoryContainer.append(button)
+    })
 }
 
 loadCategories();
