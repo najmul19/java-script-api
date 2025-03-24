9165b3e22cc029a5445e077f0af9add5ae46de98
najmul <mdnajmulislam10992@gmail.com>
2025-03-25 01:06:52 +0600

    added some condition on to data and add date with relative and absolute

--- API-Project/love-tube/Js/video.js
+++ API-Project/love-tube/Js/video.js
@@ -25,23 +25,24 @@ const displayCategories = (categories) => {
 };
 
 const cardDemo = {
-    "category_id": "1001",
-    "video_id": "aaab",
-    "thumbnail": "https://i.ibb.co/QPNzYVy/moonlight.jpg",
-    "title": "Midnight Serenade",
-    "authors": [
-        {
-            "profile_picture": "https://i.ibb.co/fDbPv7h/Noha.jpg",
-            "profile_name": "Noah Walker",
-            "verified": false
-        }
-    ],
-    "others": {
-        "views": "543K",
-        "posted_date": ""
+  category_id: "1001",
+  video_id: "aaab",
+  thumbnail: "https://i.ibb.co/QPNzYVy/moonlight.jpg",
+  title: "Midnight Serenade",
+  authors: [
+    {
+      profile_picture: "https://i.ibb.co/fDbPv7h/Noha.jpg",
+      profile_name: "Noah Walker",
+      verified: false,
     },
-    "description": "'Midnight Serenade' by Noah Walker is a soulful journey into the depths of the night, capturing the mystique and allure of a moonlit evening. With 543K views, this song brings together tender melodies and evocative lyrics, making it a favorite among listeners seeking a contemplative yet uplifting experience. Immerse yourself in this musical masterpiece and feel the calm embrace of the night."
-}
+  ],
+  others: {
+    views: "543K",
+    posted_date: "",
+  },
+  description:
+    "'Midnight Serenade' by Noah Walker is a soulful journey into the depths of the night, capturing the mystique and allure of a moonlit evening. With 543K views, this song brings together tender melodies and evocative lyrics, making it a favorite among listeners seeking a contemplative yet uplifting experience. Immerse yourself in this musical masterpiece and feel the calm embrace of the night.",
+};
 
 const displayVideos = (videos) => {
   const videoContainer = document.getElementById("videos");
@@ -50,27 +51,34 @@ const displayVideos = (videos) => {
     const videoCard = document.createElement("card");
     videoCard.classList = "card card-compact";
     videoCard.innerHTML = `
-         <figure class ="h-[200px]">
+    <figure class ="h-[200px] relative">
     <img
       src=${video.thumbnail}
       class="h-full w-full object-cover"
       alt="Shoes" />
-  </figure>
-  <div class="px-0 py-2 flex gap-2">
+      <span class="absolute right-2 bottom-2 bg-black text-white rounded p-1">${video.others.posted_date}</span>
+    </figure>
+    <div class="px-0 py-2 flex gap-2">
     <div>
-        <img class="w-10 h-10 rounded-full object-cover" src="${video.authors[0].profile_picture}" />
+        <img class="w-10 h-10 rounded-full object-cover" src="${
+          video.authors[0].profile_picture
+        }" />
     </div>
     <div>
         <h2 class="font-bold">${video.title}<h2/>
         <div class="flex items-center gap-2">
             <p class="text-gray-400">${video.authors[0].profile_name}</p>
-            <img class="w-5" src="https://img.icons8.com/?size=96&id=D9RtvkuOe31p&format=png" />
-        </div>
+            ${
+              video.authors[0].verified
+                ? ` <img class="w-5" src="https://img.icons8.com/?size=96&id=D9RtvkuOe31p&format=png" />`
+                : ""
+            }
+            </div>
 
         
         <p></p>
     </div>
-  </div>
+    </div>
         `;
     videoContainer.append(videoCard);
   });
