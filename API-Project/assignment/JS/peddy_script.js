804fb3abf183c2436baecbc244e1b1bce0ed4d3f
najmul <mdnajmulislam10992@gmail.com>
2025-03-26 05:52:33 +0600

    complete the card section and implement the button category dynamically show in the ui

---
+++ API-Project/assignment/JS/peddy_script.js
@@ -0,0 +1,111 @@
+// console.log('connected')
+
+const loadCategories = () => {
+  fetch("https://openapi.programming-hero.com/api/peddy/categories")
+    .then((res) => res.json())
+    .then((data) => displayCategories(data.categories))
+    .catch((err) => console.log(err));
+};
+const loadVideos = () => {
+  fetch("https://openapi.programming-hero.com/api/peddy/pets")
+    .then((res) => res.json())
+    .then((data) => displayVideos(data.pets))
+    .catch((err) => console.log(err));
+};
+const demo = {
+  petId: 1,
+  breed: "Golden Retriever",
+  category: "Dog",
+  date_of_birth: "2023-01-15",
+  price: 1200,
+  image: "https://i.ibb.co.com/p0w744T/pet-1.jpg",
+  gender: "Male",
+  pet_details:
+    "This friendly male Golden Retriever is energetic and loyal, making him a perfect companion for families. Born on January 15, 2023, he enjoys playing outdoors and is especially great with children. Fully vaccinated, he's ready to join your family and bring endless joy. Priced at $1200, he offers love, loyalty, and a lively spirit for those seeking a playful yet gentle dog.",
+  vaccinated_status: "Fully",
+  pet_name: "Sunny",
+};
+const displayVideos = (video) => {
+  const videoContainer = document.getElementById("videos");
+  videoContainer.innerHTML=""
+  console.log("hey :   ",video)
+  video.forEach((ele) => {
+    console.log(ele);
+    const videCard = document.createElement("card");
+    videCard.innerHTML = `
+         <figure class="h-[200px]">
+            <img  class="h-full w-full object-cover rounded-md"
+            src=${ele.image}
+            alt="pet card photoes" />
+        </figure>
+        <div class="">
+            <h2 class="card-title text-black font-bold">${ele.pet_name}</h2>
+            <div class=" flex items-center gap-1  text-gray-600">
+                <img class="h-[15px] w-[15px] " src="https://img.icons8.com/?size=100&id=12560&format=png" />
+                <p class="">Breed: ${ele.breed}</p>
+            </div>
+            <div class="flex items-center gap-1  text-gray-600">
+                <img class="h-[15px] w-[15px]" src="https://img.icons8.com/?size=100&id=12560&format=png" />
+                <p>Birth: ${ele.date_of_birth}</p>
+            </div>
+            <div class="flex items-center gap-1  text-gray-600 ">
+                <img class="h-[15px] w-[15px]" src="https://img.icons8.com/?size=100&id=12560&format=png" />
+                <p>Gender: ${ele.gender}</p>
+            </div>
+            <div class="flex items-center gap-1  text-gray-600">
+                <img class="h-[15px] w-[15px]" src="https://img.icons8.com/?size=100&id=12560&format=png" />
+                <p>Price: ${ele.price}$</p>
+            </div>
+            <hr>
+        <div class="card-actions justify-between flex">
+            
+          
+                <button class="btn text-teal-800 font-bold "><img class="h-[30px] w-[30px]" src="https://img.icons8.com/?size=48&id=82788&format=png"/></button>
+                <button class="btn text-teal-800 font-bold  ">adopt</button>
+                <button class="btn text-teal-800 font-bold  ">details</button>
+          
+        </div>
+        </div>
+        `;
+        videoContainer.append(videCard);
+  });
+};
+const displayCategories = (categories) => {
+  const categoryContainer = document.getElementById("categories");
+  categoryContainer.innerHTML = "";
+  categories.forEach((element) => {
+    // console.log(element);
+    // const button = document.createElement("btn");
+    // button.classList = "btn";
+    // button.innerText = element.category;
+    // categoryContainer.append(button)
+
+    const buttonContainer = document.createElement("div");
+    const ok = element.category;
+    const l = ok.toLowerCase();
+    
+    console.log("display cat: ",element)
+    buttonContainer.innerHTML = `
+    <button  id="btn-${element.id}" onClick="loadCategoryVideos('${l}')" class="btn category-btn">
+    ${element.category}
+    </button>
+    `;
+    categoryContainer.append(buttonContainer);
+  });
+};
+
+const loadCategoryVideos = (videoId) => {
+//   alert(videoId);
+
+console.log(videoId)
+  fetch(`https://openapi.programming-hero.com/api/peddy/category/${videoId}`)
+    .then((res) => res.json())
+    .then((data) => displayVideos(data.data))
+    .catch((err) => console.log(err));
+};
+
+
+  
+
+loadCategories();
+loadVideos();
